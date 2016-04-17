d0bc462bc782185fbc0b68ad9a54acbe3d77cad6
Bruno Augusto <bvaugusto@gmail.com>
2016-04-17 17:05:16 -0300

    teste branche 2
    teste branche 2

--- controllers/animal.js
+++ controllers/animal.js
@@ -1,7 +1,7 @@
 var Animal = require('./../modules/animal');
 
 var AnimalController = {
-	// teste
+	// teste teste 2
 	index: function(req, res){
 		Animal.find({}, function(err, user){
 			if(err)
